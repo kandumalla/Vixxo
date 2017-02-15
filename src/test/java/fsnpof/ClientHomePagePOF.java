4878a1b22b5c116fff4046f25795e5a5b3658379
Deonte Mohamed <DMohamed@VMDBITMZHF.ip-tech.com>
2017-02-15 15:36:22 -0500

    Home page bucket locator update

--- src/test/java/fsnpof/ClientHomePagePOF.java
+++ src/test/java/fsnpof/ClientHomePagePOF.java
@@ -82,9 +82,9 @@ Assert.assertTrue(HomePageTitle.getText().contains("Welcome"), "Expected {Page T
 	// Completed Requests Bucket
 	@FindBy(id = "ctl00_ContentPlaceHolder1_RepeaterReqCat_ctl02_ImageExpCol")
 	WebElement CompletedRequestsbtn;
-	@FindBy(id = "ctl00_ContentPlaceHolder1_RepeaterReqCat_ctl02_RepeaterReqCatDetails_ctl01_HyperlinkCatDet")
+	@FindBy(xpath = "//*[contains(text(),'Web Document In Transit')]")
 	WebElement WebDocumentsinTransit;
-	@FindBy(id = "ctl00_ContentPlaceHolder1_RepeaterReqCat_ctl02_RepeaterReqCatDetails_ctl00_HyperlinkCatDet")
+	@FindBy(xpath = "//*[contains(text(),'To Be Billed')]")
 	WebElement ToBeBilled;
 	@FindBy(id = "ctl00_ContentPlaceHolder1_RepeaterReqCat_ctl02_RepeaterReqCatDetails_ctl02_lbCompleted_Billed")
 	WebElement Billed;
