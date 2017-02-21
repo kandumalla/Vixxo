d02fc2f4cfa18d55363231fb010636bca1e1ad91
Dlmohamed07 <Deonte.Mohamed@vixxo.com>
2017-02-20 23:49:50 -0500

    Update Utilities.java

--- src/test/java/fsnpof/Utilities.java
+++ src/test/java/fsnpof/Utilities.java
@@ -14,8 +14,8 @@ public class Utilities {
 	 
 	public static WebDriver driver;
 	public static void webdrive (){
-		//System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\dmohamed\\Documents\\Testing Environment\\Testing Environment\\Web Drivers\\chromedriver_win32 (1)\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\dmohamed\\Documents\\Testing Environment\\Testing Environment\\Web Drivers\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver chromedriver = new ChromeDriver();
 		driver= chromedriver;
 		
